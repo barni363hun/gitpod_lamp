4048c77dacf167ad39139782f00f2aa2b8ffd739
apolopena <3060702+apolopena@users.noreply.github.com>
2022-03-14 19:33:47 -0700

    👌 IMPROVE: run browser-sync as a webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// Simple default webpack configuration to proxy apache with browser-sync for hot reloading
+// on Gitpod with dynamically served PHP files
+
+const { execSync } = require('child_process');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const PROXY_PORT=8001
+const BS_PORT = 3005
+const GP_URL = execSync(`gp url ${BS_PORT}`) 
+
+module.exports = {
+  mode: "development",
+  entry: {},
+  output: {},
+  plugins: [
+    new BrowserSyncPlugin(
+      {
+        ui: false,
+        proxy: {
+          target: `http://localhost:${PROXY_PORT}`
+        },
+        // Add other file types to be hot reloaded here in this array 
+        files: ["**/*.php"],
+        notify: true,
+        port: BS_PORT,
+        socket: {
+          domain: GP_URL
+        }
+      }
+    )
+  ]
+}
